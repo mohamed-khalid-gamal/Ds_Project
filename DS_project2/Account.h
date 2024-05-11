43f5eeb3e8e719cb3ad808358586280ba08c3390
Ahmad Zo-ElFakkar <ahmadzoelfakkar@gmail.com>
2024-05-11 03:57:19 +0300

    Fixed almost all problems program should be working fine now.

--- DS_project2/Account.h
+++ DS_project2/Account.h
@@ -10,5 +10,6 @@ public:
 	void logIn(std::unordered_map<std::string, User>& allUsers, std::stack<Transaction>& allTransactions);
 	void registerAccount(std::unordered_map<std::string, User>& allUsers);
 	void forgetPassword(std::unordered_map<std::string, User>& allUsers);
+	std::string hashText(std::string);
 };
 
